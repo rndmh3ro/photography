fe3d310d2fde48e9ecbd9796a32f749f9ca91721
basti <sebastian.gumprich@38.de>
2013-01-06 19:41:45 +0100

    Signed-off-by: basti <sebastian.gumprich@38.de>

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+  <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+  <!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+  <!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+ 
+  <head> 
+    <meta charset="UTF-8"> 
+    
+    <!-- TITLE -->
+    <title>Photographie-Blog</title>
+    
+    <!-- META -->
+    <meta name="description" content="Photographie-Blog" />
+    
+    <!-- STYLES -->
+    <link rel="stylesheet" href="resource/css/style.css" />
+    
+    <!-- MODERNIZR -->
+    <script src="resource/js/libs/modernizr.min.js"></script>
+  </head>
+
+  <body>
+    <div id="container"> 
+      <!-- HEADER -->
+      <header id="header">
+        <h1>Sebastian Gumprich Photographie</h1>
+        <hr>
+      </header>
+      
+      <!-- CONTENT -->
+      <div id="content" role="main">
+    </div>
+    <footer>
+      <?php
+        include("./footer.php");
+      ?>
+    </footer>
+    
+    <!-- SCRIPTS -->
+    <script src="http://code.jquery.com/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="resource/js/libs/jquery-1.8.3.min.js">\x3C/script>')</script>
+    <!--[if (gte IE 6)&(lte IE 8)]><script src="resource/js/libs/selectivizr.min.js"></script><![endif]-->
+    <script src="resource/js/plugins.js"></script>
+    <script src="resource/js/global.js"></script>
+  </body>
+</html>
