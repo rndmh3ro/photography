e6dfc0bef1ea943a212940f702a27326191da233
basti <sebastian.gumprich@38.de>
2013-02-03 12:25:42 +0100

    signed off

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 <!-- Piwik -->
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.sebastian-gumprich.de/" : "http://piwik.sebastian-gumprich.de/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='" + pkBaseURL + "js/' type='text/javascript'%3E%3C/script%3E"));
 </script><script type="text/javascript">
 try {
 var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
