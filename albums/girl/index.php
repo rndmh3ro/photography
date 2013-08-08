4f7f7d68e5d37ce39ba49252ce7bb218a351cb5b
basti <Sebastian Gumprich>
2013-08-08 21:41:19 +0200

    added new album

---
+++ albums/girl/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<!doctype html>
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+<!--[if lte IE 9]><link rel="stylesheet" href="resource/css/ie.css" type="text/css" media="screen" /><![endif]-->
+
+<head> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">  
+  <title>Sebastian Gumprich Photographie</title>
+  <meta name="description" content="Sebastian Gumprich Photographie" />
+  <script src="../../resource/js/libs/jquery-1.8.3.min.js"></script>
+  <link rel="stylesheet" href="../../resource/css/fancybox/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
+  <script type="text/javascript" src="../../resource/js/jquery.fancybox.pack.js?v=2.1.4"></script>
+  <link rel="stylesheet" href="../../resource/css/1140.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="../../resource/css/style.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="../../resource/js/libs/css3-mediaqueries.js"></script>
+  <script src="../../resource/js/libs/modernizr.min.js"></script>
+</head>
+<body>
+    <script type="text/javascript">$(document).ready(function() {$(".fancybox").fancybox();});</script>
+<div class="wrap"> 
+  <!-- HEADER -->
+  <div class="header">
+    <header>
+      <?php
+      include("../../header.php");
+      ?>
+    </header>
+  </div> 
+  <!-- CONTENT -->
+    <div class="container fade_in">
+     <div class="grid_6">
+        <div class="center"><a class="fancybox" rel="group" href="IMG_9451.jpg"><img class="thumb zoom" src="tn/tn_IMG_9451.jpg" alt="Girlin bed"></a>
+        </div>
+      </div>
+      <div class="grid_6">
+        <div class="center"><a class="fancybox" rel="group" href="IMG_9462.jpg"><img class="thumb zoom" src="tn/tn_IMG_9462.jpg" alt="Girl in bed"></a>
+        </div>
+      </div>
+       <div class="grid_6">
+        <div class="center"><a class="fancybox" rel="group" href="IMG_9472.jpg"><img class="thumb zoom" src="tn/tn_IMG_9472.jpg" alt="Girl in bed"></a>
+        </div>
+      </div>  
+       <div class="grid_6">
+        <div class="center"><a class="fancybox" rel="group" href="IMG_9484.jpg"><img class="thumb zoom" src="tn/tn_IMG_9484.jpg" alt="Girl in bed"></a>
+        </div>
+      </div>         
+    </div>
+  </div>
+  <!-- FOOTER -->
+  <div class="footer">
+    <footer>
+      <?php
+      include("../../footer.php");
+      ?>
+    </footer>
+  </div>
+<!--[if (gte IE 6)&(lte IE 8)]><script src="resource/js/libs/selectivizr.min.js"></script><![endif]-->
+</body>
+</html>
